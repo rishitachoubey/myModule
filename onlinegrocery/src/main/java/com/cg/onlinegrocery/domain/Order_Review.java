c3bf68ee5293c84749166d47d8c987068da99122
Rishita Choubey <46817010+rishitachoubey@users.noreply.github.com>
2021-02-20 21:06:05 +0530

    Update and rename OrderReview.java to Order_Review.java

--- onlinegrocery/src/main/java/com/cg/onlinegrocery/domain/Order_Review.java
+++ onlinegrocery/src/main/java/com/cg/onlinegrocery/domain/Order_Review.java
@@ -1,34 +1,19 @@
 package com.cg.onlinegrocery.domain;
 
 import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-
 
 @Entity
 @Table(name = "Order_Review")
-public class OrderReview {
+public class Order_Review {
 
 	@Id
-	@GeneratedValue
-	@Column(name = "Order_id")
-	private Integer Order_id;
+	@NotNull(message= "Order id should not be null")
+	private int Order_id;
 
 	@NotNull(message = "Cost should not be null")
 	private Double OrderCost;
@@ -36,25 +21,20 @@ public class OrderReview {
 	@Column(name = "OrderDate")
 	private String OrderDate;
 	
-	
 	@Column(name = "OrderAddress")
 	private String OrderAddress;
 	
 	@Column(name = "Coupon")
-	private double Coupon;
+	private int Coupon;
 
-	@JsonIgnore
-	@ManyToOne(cascade = CascadeType.PERSIST)
-	@JoinColumn(name="id")
-	private OrderReview order;
+//
+//	@Valid
+//	@OneToMany(cascade = CascadeType.ALL,fetch=FetchType.EAGER)
+//	@JoinColumn(name = "order_id")
+//	@Fetch(value=FetchMode.SELECT)
+//	private List<Order> orders;
 
-	@Valid
-	@OneToMany(cascade = CascadeType.ALL,fetch=FetchType.EAGER)
-	@JoinColumn(name = "order_id")
-	@Fetch(value=FetchMode.SELECT)
-	private List<OrderReview> orders;
-
-	public OrderReview(int Order_id, double OrderCost, String OrderDate, String OrderAddress, double Coupon, List<OrderReview> orders) {
+	public Order_Review(int Order_id, double OrderCost, String OrderDate, String OrderAddress, int Coupon, List<Order_Review> orders) {
 		super();
 		this.Order_id = Order_id;
 		this.OrderCost = OrderCost;
@@ -62,7 +42,14 @@ public class OrderReview {
 		this.OrderAddress = OrderAddress;
 		this.Coupon = Coupon;
 	}
+	
    
+	public Order_Review() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
 	public void setOrder_id(int Order_id) {
 		this.Order_id = Order_id;
 	}
@@ -87,11 +74,22 @@ public class OrderReview {
 	public String getOrderAddress() {
 		return OrderAddress;
 	}
+	public double getCoupon() {
+		return Coupon;
+	}
+	public void setCoupon(int Coupon) {
+		this.Coupon = Coupon;
+	}
 
 	@Override
 	public String toString() {
 		return "Order [Order_id=" + Order_id + ", OrderCost=" + OrderCost + ", OrderDate=" + OrderDate
 				+ ", OrderAddress=" + OrderAddress + ", Coupon=" + Coupon + "]";
 	}
+	
+
+
+
 
 }
+
